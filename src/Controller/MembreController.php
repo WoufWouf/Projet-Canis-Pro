570e6e139cc7d38c941aa3489f1f22924d0c77dd
Annaelle972 <140344538+Annaelle972@users.noreply.github.com>
2026-03-11 16:26:46 +0000

    V12 = V11 + Vue Espace Membre Code commentés

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProprietaireRepository;
 
 final class MembreController extends AbstractController
 {
@@ -15,4 +16,22 @@ final class MembreController extends AbstractController
             'controller_name' => 'MembreController',
         ]);
     }
+
+  /*   #[Route('/espace-personnel', name: 'espace_personnel')]
+    public function espacePearsonnel(ProprietaireRepository $proprietaireRepository): Response
+    {
+        $proprietaires = $proprietaireRepository->findAll();
+        
+        if (empty($proprietaires)) {
+            throw $this->createNotFoundException('Aucun propriétaire trouvé');
+        }
+
+        // Pour le test, on prend le premier propriétaire
+        $proprietaire = $proprietaires[0];
+
+        return $this->render('membre/espace_personnelle.html.twig', [
+            'proprietaire' => $proprietaire,
+            'chiens' => $proprietaire->getChiens(),
+        ]);
+    } */
 }
