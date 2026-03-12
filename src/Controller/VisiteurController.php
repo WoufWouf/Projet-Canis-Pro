3067a69cbbbdddf85ed1ffa5ae21b3d8664663e6
Annaelle972 <140344538+Annaelle972@users.noreply.github.com>
2026-03-12 16:23:29 +0000

    V21 = V20 + Ajout fonction toute les seances

--- src/Controller/VisiteurController.php
+++ src/Controller/VisiteurController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\SeanceRepository;
 
 final class VisiteurController extends AbstractController
 {
@@ -13,6 +14,16 @@ final class VisiteurController extends AbstractController
     {
         return $this->render('visiteur/index.html.twig', [
             'controller_name' => 'VisiteurController',
+        ]);
+    }
+
+        #[Route('/visiteur/seance', name: 'app_visiteur_seance')]
+    public function seance(SeanceRepository $seanceRepository): Response
+    {
+        $seance = $seanceRepository->findAll();
+
+        return $this->render('visiteur/seance.html.twig', [
+            'seances' => $seance,
         ]);
     }
 }
